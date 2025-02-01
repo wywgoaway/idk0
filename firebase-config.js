9236f56da1495ce0b335629e1631c5225f61a0de
wywgoaway <wywgoaway@users.noreply.github.com>
2025-02-01 14:03:15 +0000

    deploy: 7d3e81e89817d70d55f4a732272f85f0c3bb0fe7

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyCSmicQncJhWtvEekSLNQtvWM6fFr0cuRM',
+  authDomain: 'html-7fd9b.firebaseapp.com',
+  projectId: 'html-7fd9b',
+  storageBucket: 'html-7fd9b.appspot.com',
+  messagingSenderId: '1022361191925',
+  appId: '1:1022361191925:web:6ea5f58d8c160bdbaa0cbc'
 }; export default firebaseConfig;
